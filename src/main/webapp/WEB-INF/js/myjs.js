1577c762925bce62e0206fcd8d6537bcb49fe590
mai ciu s <maicius@outlook.com>
2017-04-22 14:00:00 +0800

    add test4

--- src/main/webapp/WEB-INF/js/myjs.js
+++ src/main/webapp/WEB-INF/js/myjs.js
@@ -161,3 +161,25 @@ function third(){
         myChart.setOption(option, true);
     }
 }
+
+function simpleChart() {
+
+    var myChart = echarts.init(document.getElementById('test4'));
+    myChart.setOption({
+        series : [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                data:[
+                    {value:235, name:'视频广告'},
+                    {value:274, name:'联盟广告'},
+                    {value:310, name:'邮件营销'},
+                    {value:335, name:'直接访问'},
+                    {value:400, name:'搜索引擎'}
+                ]
+            }
+        ]
+    })
+
+}
